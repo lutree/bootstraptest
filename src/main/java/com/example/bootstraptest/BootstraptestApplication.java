05ef32925b2a9f39409604ab5374d2e3a0151fea
lutree <lutree@outlook.kr>
2023-02-03 22:21:36 +0900

    Initial commit

---
+++ src/main/java/com/example/bootstraptest/BootstraptestApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootstraptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstraptestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootstraptestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bootstraptest/BootstraptestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootstraptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstraptestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
